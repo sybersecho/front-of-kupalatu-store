97530257427d8d349ae8efbc9eccf4df0b9e8736
sybresecho <sybers_echo@yahoo.co.id>
2018-03-11 15:04:03 +0700

    search inline

---
+++ src/app/content/products/product-list/search-inline/search-inline.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-inline',
+  templateUrl: './search-inline.component.html',
+  styleUrls: ['./search-inline.component.css']
+})
+export class SearchInlineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
