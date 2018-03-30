f70dcb36938e0ff4a3aafefbb5b82203535d7554
sybresecho <sybers_echo@yahoo.co.id>
2018-03-30 10:27:06 +0700

    user feature

---
+++ src/app/admin/users/components/user-list/search-user-inline/search-user-inline.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-user-inline',
+  templateUrl: './search-user-inline.component.html',
+  styleUrls: ['./search-user-inline.component.css']
+})
+export class SearchUserInlineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/admin/users/components/user-list/user-form/user-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.css']
+})
+export class UserFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+
+  }
+
+}
